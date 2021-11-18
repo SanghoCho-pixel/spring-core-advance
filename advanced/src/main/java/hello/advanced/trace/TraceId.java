7850d6788c3a704f1c63a7de4159efec763c71e8
SanghoCho <shxho0476@gmail.com>
2021-11-18 11:53:28 +0900

    feat:직접 log trace 구현 및 적용

--- advanced/src/main/java/hello/advanced/trace/TraceId.java
+++ advanced/src/main/java/hello/advanced/trace/TraceId.java
@@ -18,15 +18,15 @@ public class TraceId {
 
     }
 
-    private String createdId() {
+    public String createdId() {
         return UUID.randomUUID().toString().substring(0, 8);
     }
 
-    private TraceId createNextId(){
+    public TraceId createNextId(){
         return new TraceId(id, level + 1);
     }
 
-    private TraceId createPreviousId(){
+    public TraceId createPreviousId(){
         return new TraceId(id, level - 1);
     }
 
